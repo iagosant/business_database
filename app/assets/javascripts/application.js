d5207f0e54beeeb85b855c14e921552483a8da29
iagosant <sgarzaceja@gmail.com>
2017-06-17 16:43:56 -0400

    materialize working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require materialize-sprockets
