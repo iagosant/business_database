b74099bc12e08539f05e387a88dc993fa61d4e4d
iagosant <sgarzaceja@gmail.com>
2017-06-19 14:05:44 -0400

    ransack is awesome

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,13 +1,13 @@
 class Business < ActiveRecord::Base
 validates_uniqueness_of :name, scope: [:start_date, :address, :mailing_addres]
 
-def self.search(search)
-  if search
-  where('name LIKE ?', "%#{search}%")
-  else
-  scoped
-  end
-end
+# def self.search(search)
+#   if search
+#   where('name LIKE ?', "%#{search}%")
+#   else
+#   scoped
+#   end
+# end
 
 
 def self.import(file)
